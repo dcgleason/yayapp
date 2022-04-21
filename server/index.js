c74664fcf9ffddcd7e229bfcba68cd592c020f61
Danny Gleason <danny.c.gleason@gmail.com>
2022-04-21 07:54:36 -0400

    changed configuration for SSR

--- server/index.js
+++ server/index.js
@@ -10,11 +10,63 @@ const axios = require('axios');
 const pg = require('pg')
 var id_queue = []
 var array = []
+const { createServer: createViteServer } = require('vite')
 
 let dotenv = require('dotenv');
 
 // if .env file is located in root directory
 dotenv.config()
+
+async function createServer() {
+
+const vite = await createViteServer({
+
+  server: { middlewareMode: 'ssr' }
+})
+// use vite's connect instance as middleware
+app.use(vite.middlewares)
+
+app.use('*', async (req, res) => {
+  const url = req.originalUrl
+
+  try {
+    // 1. Read index.html
+    let template = fs.readFileSync(
+      path.resolve(__dirname, '../index.html'),
+      'utf-8'
+    )
+
+    // 2. Apply Vite HTML transforms. This injects the Vite HMR client, and
+    //    also applies HTML transforms from Vite plugins, e.g. global preambles
+    //    from @vitejs/plugin-react
+    template = await vite.transformIndexHtml(url, template)
+
+    // 3. Load the server entry. vite.ssrLoadModule automatically transforms
+    //    your ESM source code to be usable in Node.js! There is no bundling
+    //    required, and provides efficient invalidation similar to HMR.
+    const { render } = await vite.ssrLoadModule('../src/main.jsx')
+
+    // 4. render the app HTML. This assumes entry-server.js's exported `render`
+    //    function calls appropriate framework SSR APIs,
+    //    e.g. ReactDOMServer.renderToString()
+    const appHtml = await render(url)
+
+    // 5. Inject the app-rendered HTML into the template.
+    const html = template.replace(`<!--ssr-outlet-->`, appHtml)
+
+    // 6. Send the rendered HTML back.
+    res.status(200).set({ 'Content-Type': 'text/html' }).end(html)
+  } catch (e) {
+    // If an error is caught, let Vite fix the stracktrace so it maps back to
+    // your actual source code.
+    vite.ssrFixStacktrace(e)
+    next(e)
+  }
+})
+
+}
+
+createServer();
 
 app.use(cors())
 app.use(express.json())
